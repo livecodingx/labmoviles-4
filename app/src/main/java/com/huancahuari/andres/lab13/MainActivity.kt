f38f483cc13199c1cb9897c067f1e598801c747d
andre <andresalexanderhq@gmail.com>
2024-12-07 17:17:11 -0500

    second commit

--- app/src/main/java/com/huancahuari/andres/lab13/MainActivity.kt
+++ app/src/main/java/com/huancahuari/andres/lab13/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.huancahuari.andres.lab13
 
+import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
@@ -12,6 +14,7 @@ import androidx.camera.core.ImageCaptureException
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.content.ContextCompat
+import androidx.core.net.toUri
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.snackbar.Snackbar
@@ -28,7 +31,6 @@ class MainActivity : AppCompatActivity() {
     private lateinit var cameraSelector: CameraSelector
     private var imageCapture: ImageCapture? = null
     private lateinit var imgCaptureExecutor: ExecutorService
-
     private val cameraPermissionResult =
         registerForActivityResult(ActivityResultContracts.RequestPermission()) { permissionGranted ->
             if (permissionGranted) {
@@ -41,80 +43,75 @@ class MainActivity : AppCompatActivity() {
                 ).show()
             }
         }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-
-        // Solicitar permiso para la cámara
+        cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+        cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+        imgCaptureExecutor = Executors.newSingleThreadExecutor()
         cameraPermissionResult.launch(android.Manifest.permission.CAMERA)
 
-        // Configuración del botón para tomar la foto
-        binding.imgCaptureBtn.setOnClickListener {
+        binding.imgCaptureBtn.setOnClickListener{
             takePhoto()
+        }
+        binding.switchBtn.setOnClickListener{
+            cameraSelector = if (cameraSelector == CameraSelector.DEFAULT_BACK_CAMERA) {
+                CameraSelector.DEFAULT_FRONT_CAMERA
+            } else {
+                CameraSelector.DEFAULT_BACK_CAMERA
+            }
+            startCamera()
+        }
+        binding.galleryBtn.setOnClickListener {
+            val intent = Intent(this, GalleryActivity::class.java)
+            startActivity(intent)
         }
     }
 
     private fun startCamera() {
-        imgCaptureExecutor = Executors.newSingleThreadExecutor()
 
-        // Obtener el ProcessCameraProvider
-        cameraProviderFuture = ProcessCameraProvider.getInstance(this)
-
+        val preview = Preview.Builder().build().also{
+            it.setSurfaceProvider(binding.preview.surfaceProvider)
+        }
         cameraProviderFuture.addListener({
+            val cameraProvider = cameraProviderFuture.get()
+            imageCapture = ImageCapture.Builder().build()
             try {
-                val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
-
-                cameraSelector = CameraSelector.Builder()
-                    .requireLensFacing(CameraSelector.LENS_FACING_BACK)
-                    .build()
-                val preview = Preview.Builder().build().also {
-                    it.setSurfaceProvider(binding.preview.surfaceProvider)
-                }
-
-                imageCapture = ImageCapture.Builder().build()
-
                 cameraProvider.unbindAll()
                 cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageCapture)
-
-            } catch (e: ExecutionException) {
-                Log.e(TAG, "Error al obtener el CameraProvider", e)
-            } catch (e: InterruptedException) {
-                Log.e(TAG, "Error al obtener el CameraProvider", e)
+            } catch (e: Exception) {
+                Log.d(TAG, "Error al obtener el CameraProvider", e)
             }
-        }, ContextCompat.getMainExecutor(this)) // Ejecutamos en el hilo principal
+        }, ContextCompat.getMainExecutor(this))
     }
 
     private fun takePhoto() {
-        val imageCapture = imageCapture ?: return
-        val photoFile = File(externalMediaDirs.first(), "photo_${System.currentTimeMillis()}.jpg")
-        val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
+        imageCapture?.let{
+            val fileName = "JPEG_${System.currentTimeMillis()}"
+            val file = File(externalMediaDirs[0], fileName)
+            val outputFileOptions = ImageCapture.OutputFileOptions.Builder(file).build()
 
-        imageCapture.takePicture(
-            outputOptions,
-            imgCaptureExecutor,
-            object : ImageCapture.OnImageSavedCallback {
-                override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
-                    val savedUri = outputFileResults.savedUri ?: Uri.fromFile(photoFile)
-                    Snackbar.make(binding.root, "Photo saved to $savedUri", Snackbar.LENGTH_SHORT).show()
-                    Toast.makeText(applicationContext, "Photo saved!", Toast.LENGTH_SHORT).show()
-                }
+            it.takePicture(
+                outputFileOptions,
+                imgCaptureExecutor,
+                object : ImageCapture.OnImageSavedCallback {
+                    override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+                        Log.i(TAG, "the image has been saved in ${file.toUri()}")
+                    }
 
-                override fun onError(exception: ImageCaptureException) {
-                    Snackbar.make(binding.root, "Error taking photo: ${exception.message}", Snackbar.LENGTH_LONG).show()
-                    Log.e(TAG, "Error taking photo", exception)
+                    override fun onError(exception: ImageCaptureException) {
+                        Toast.makeText(binding.root.context,
+                            "Error taking photo: ${exception.message}",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        Log.d(TAG, "Error taking photo", exception)
+                    }
                 }
-            }
-        )
+            )
+        }
     }
-
     companion object {
         private const val TAG = "MainActivity"
     }
